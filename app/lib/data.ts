01e84aedd306297c793d1eb107cc66c6744a37e9
Gerardo Camorlinga Jr <gerardo.camorlinga@guardmetrics.com>
2024-06-20 12:00:55 -0500

    moved noStore outside try/catch block

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -14,14 +14,13 @@ import { formatCurrency } from './utils';
 export async function fetchRevenue() {
   // Add noStore() here to prevent the response from being cached.
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
-
+  noStore();
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
     // console.log('Fetching revenue data...');
     // await new Promise((resolve) => setTimeout(resolve, 3000));
-    noStore();
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
